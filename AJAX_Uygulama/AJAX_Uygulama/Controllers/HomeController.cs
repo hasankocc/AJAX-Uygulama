6a3277cfa1bcd955a0a223e68d714115aa864b0a
Hasan Koç <posthasankoc@gmail.com>
2018-12-30 22:27:32 +0300

    Ajax

---
+++ AJAX_Uygulama/AJAX_Uygulama/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using AJAX_Uygulama.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AJAX_Uygulama.Controllers
+{
+    public class HomeController : Controller
+    {
+        studentGradeEntities db = new studentGradeEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult All() 
+        {
+            List<Student> model = db.Students.ToList();
+            return PartialView("_Student",model);
+        }
+
+        public PartialViewResult Top3()
+        {
+            List<Student> model = db.Students.OrderByDescending(x => x.grade).Take(3).ToList();
+            return PartialView("_Student" , model);
+        }
+
+        public PartialViewResult Bottom3()
+        {
+            List<Student> model = db.Students.OrderBy(x => x.grade).Take(3).ToList();
+            return PartialView("_Student", model);
+        }
+
+    }
+}
